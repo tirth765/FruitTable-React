907ec569f31cc2d6417fa8347f7b7a9df099a22a
tirth765 <tirth.patel6521@gmail.com>
2025-01-06 08:20:30 +0530

    count

--- src/admin/Container/Counter.js
+++ src/admin/Container/Counter.js
@@ -1,9 +1,11 @@
 import React, { useReducer } from 'react'
 import { decrement, increment } from '../../redux/action/counter.action';
 import { Button } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function Counter(dispatch) {
+export default function Counter() {
+
+  const dispatch = useDispatch()
 
   const handleIncrement = () => {
     dispatch(increment());
@@ -16,19 +18,14 @@ export default function Counter(dispatch) {
   const c = useSelector ((state => state.count))
   console.log(c);
   
-  
-  // const [state, dispatch] = useReducer(handleCounter, initializState);
-
-
   return (
     <>
 
-    {/* <div>Count: {state.coun}</div> */}
     
     <div>Counter:</div>
-    <Button onclick={handleIncrement}>+</Button>
+    <Button onClick={handleIncrement}>+</Button>
     <span>{c.count}</span>
-    <Button onclick={handleDecrement}>-</Button>
+    <Button onClick={handleDecrement}>-</Button>
 
     </>
   )
