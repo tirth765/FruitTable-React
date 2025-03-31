44b6e0c0855f8cf6aebe339171cce67692abf9f2
tirth765 <tirth.patel6521@gmail.com>
2025-03-31 19:15:25 +0530

    home work

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { rootReducer } from "./reducer"
-import { CategorySlice } from "./Slice/CategorySlice"
 
 export const createStore = () => {
     const store = configureStore ({
