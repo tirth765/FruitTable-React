9d5774f98b7a962ea8446efd039ee3d00f0b5c31
tirth765 <tirth.patel6521@gmail.com>
2025-04-29 09:13:49 +0530

    f

--- src/Utils/Base.js
+++ src/Utils/Base.js
@@ -1,3 +1,3 @@
-export const IMG_URL = "http://localhost:8000/"
-export const BASE_URL = "http://localhost:8000/api/v1/"
+export const IMG_URL = "https://leran-node-2.vercel.app/"
+export const BASE_URL = "https://leran-node-2.vercel.app/api/v1/"
 export const CHAT_URL = "http://localhost:4000/"
